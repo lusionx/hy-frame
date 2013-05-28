3dfb83998be85de34676de7cfd1ae317d7dd3990
lusionx <lusionx@gmail.com>
2013-05-28 09:45:01 +0800

    add ijson

---
+++ HY.Frame.Core/Registration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HY.Frame.Core
+{
+    public class Registration
+    {
+        public static void ToJSON(Type ty, IJSON i)
+        {
+        }
+    }
+}
