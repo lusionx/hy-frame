3ad2c01273938068e32ec4f8c5b8a68b8deae548
lusionx <lusionx@gmail.com>
2013-05-31 12:02:05 +0800

    Signed-off-by: lusionx <lusionx@gmail.com>

---
+++ HY.Auth/AuthedUser.cs
@@ -0,0 +1,36 @@
+﻿using HY.Frame.Core.Toolkit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HY.Auth
+{
+    public class AuthedUser : ConfigBase
+    {
+        protected override string ConfigFilePath
+        {
+            get
+            {
+                return System.Web.HttpContext.Current.Server.MapPath("~/App_Data/Auth.xml");
+            }
+        }
+
+        public IList<string> Roles { get; set; }
+
+        public AuthedUser(IEnumerable<string> roles)
+        {
+            Roles = roles.Select(a => a.Trim()).ToList();
+        }
+
+        public AuthedUser(string role)
+        {
+            Roles = role.Split(';', ',', ' ').Where(a => !string.IsNullOrEmpty(a)).ToList();
+        }
+
+        public List<LinkNode> GetNodes()
+        {
+            return null;
+        }
+    }
+}
---
+++ HY.Auth/LinkNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HY.Auth
+{
+    public class LinkNode
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public List<LinkNode> Children { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
